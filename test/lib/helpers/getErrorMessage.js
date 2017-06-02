2cb2060f743652f581d50f36b36d541bd5865812
dapenguin <anthony.jeffery@littleblueweb.com>
2017-06-02 13:23:19 +0100

    Added middleware for rendering the error view.

---
+++ test/lib/helpers/getErrorMessage.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+const getErrorMessage = require('./../../../lib/helpers/getErrorMessage');
+
+describe('helpers/getErrorMessage', () => {
+    it('should return an array', () => {
+        const errorMessage = getErrorMessage(404);
+        expect(errorMessage).to.be.an.array;
+    });
+
+    it('should get a specific error message when it has one for the given status code', () => {
+        const errorMessage = getErrorMessage(404);
+        expect(errorMessage[0]).to.equal('The page you are trying to access does not exist.');
+    });
+
+    it('should get a more generic error message when it cannot find a specific error message for the given status code', () => {
+        const errorMessage = getErrorMessage(444);
+        expect(errorMessage[0]).to.equal('The page you are trying to access is unavailable.');
+    });
+});
+
+ 
