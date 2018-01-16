91ff20c4be594c3e9347f4011418840e948cebcf
Tye, Thurston <Thurston.Tye@ft.com>
2018-01-16 16:43:59 +0000

    fix spaces  🐿 v2.5.16

--- test/lib/fixtures/licenceSeatHolders.js
+++ test/lib/fixtures/licenceSeatHolders.js
@@ -4,12 +4,12 @@ module.exports = {
 	'returnedResourceCount': 1,
 	'totalResourceCount': 1,
 	'seatHolders': [
-	  {
-		'id': '00000000-0000-0000-0000-000000000003',
-		'lastName': 'TestLN',
-		'firstName': 'TestFN',
-		'email': 'an email address',
-		'joinedDate': '2016-10-21T13:29:22.412Z'
-	  }
+		{
+			'id': '00000000-0000-0000-0000-000000000003',
+			'lastName': 'TestLN',
+			'firstName': 'TestFN',
+			'email': 'an email address',
+			'joinedDate': '2016-10-21T13:29:22.412Z'
+		}
 	]
-  };
+};
