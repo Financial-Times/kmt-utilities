ffe4a13bd3564dcbd39ba1d8059da64426a1ce3e
Thurston Tye <thurston.tye@ft.com>
2018-01-19 10:25:30 +0000

    Update to call LDS instead of ALS for allocatedSeat number (#40)
    * Update to call LDS instead of ALS for allocatedSeat number
     🐿 v2.5.16
    
    * fix spaces
     🐿 v2.5.16
    
    * semi colon
     🐿 v2.5.16
    
    * fix spaces
     🐿 v2.5.16
    
    * set max limit on request rather than default
     🐿 v2.5.16

---
+++ test/lib/fixtures/licenceSeatHolders.js
@@ -0,0 +1,15 @@
+module.exports = {
+	'offset': 0,
+	'limit': 50000,
+	'returnedResourceCount': 1,
+	'totalResourceCount': 1,
+	'seatHolders': [
+		{
+			'id': '00000000-0000-0000-0000-000000000003',
+			'lastName': 'TestLN',
+			'firstName': 'TestFN',
+			'email': 'an email address',
+			'joinedDate': '2016-10-21T13:29:22.412Z'
+		}
+	]
+};
