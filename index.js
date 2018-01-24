e670924a81502a2df13bfda5a719897e04dba49e
Simon Legg <leggsimon@gmail.com>
2018-01-24 12:09:09 +0000

    export more clearly

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-'use strict';
-
-const fs = require('fs');
-const paths = [
-	`${__dirname}/lib/middleware`,
-	`${__dirname}/lib/helpers`,
-	`${__dirname}/lib`
-];
-
-paths.forEach(function (path) {
-	fs.readdirSync(path).forEach(function (file) {
-			if (~file.indexOf('.js')) {
-				module.exports[file.replace(/\.js/, '')] = require(`${path}/${file}`);
-			}
-	});
-});
+module.exports = {
+	bodyParser: require('./lib/middleware/bodyParser.js'),
+	checkLicenceStatus: require('./lib/middleware/checkLicenceStatus.js'),
+	checkMaintenanceStatus: require('./lib/middleware/checkMaintenanceStatus.js'),
+	errors: require('./lib/middleware/errors.js'),
+	getAuthToken: require('./lib/middleware/getAuthToken.js'),
+	getLicenceId: require('./lib/middleware/getLicenceId.js'),
+	getLicenceList: require('./lib/middleware/getLicenceList.js'),
+	getLicenceSeatsInfo: require('./lib/middleware/getLicenceSeatsInfo.js'),
+	getListOfLicences: require('./lib/middleware/getListOfLicences.js'),
+	getUserList: require('./lib/middleware/getUserList.js'),
+	isAdminSession: require('./lib/middleware/isAdminSession.js'),
+	isAdminUser: require('./lib/middleware/isAdminUser.js'),
+	middlewareCombos: require('./lib/middleware/middlewareCombos.js'),
+	myFtLicenceRegistration: require('./lib/middleware/myFtLicenceRegistration.js'),
+	redirectDefaultLicence: require('./lib/middleware/redirectDefaultLicence.js'),
+	redirectToExistingSession: require('./lib/middleware/redirectToExistingSession.js'),
+	verifySession: require('./lib/middleware/verifySession.js'),
+	cookieHandler: require('./lib/helpers/cookieHandler.js'),
+	getErrorMessage: require('./lib/helpers/getErrorMessage.js'),
+	isLicenceInList: require('./lib/helpers/isLicenceInList.js'),
+	licenceListSort: require('./lib/helpers/licenceListSort.js'),
+	config: require('./lib/config.js'),
+	initCookieSession: require('./lib/initCookieSession.js'),
+	logger: require('./lib/logger.js'),
+	uriConstructor: require('./lib/uriConstructor.js'),
+};
